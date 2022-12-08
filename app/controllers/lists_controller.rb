3a0cc89d392f2eb46247f04e4258814ea2b8aaa2
Sneha Sunny <snehasunny1997@gmail.com>
2022-12-08 22:59:22 +0800

    Changed the model of the list and movies

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -3,7 +3,7 @@ class ListsController < ApplicationController
     @lists = List.all
   end
 
-  def method_name
-    
+  def show
+    @lists = List.find(params[:id])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,20 +1,14 @@
 5.times do
   movie = Movie.create(
-    title: Faker::Movies::HarryPotter.book,
-    overview: Faker::Movies::HarryPotter.quote,
+    title: Faker::Movie.title,
+    overview: Faker::Movie.quote,
     rating: Faker::Number.between(from: 0.0, to: 10.0).round(2)
   )
 end
 
-4.times do
-  list = List.create(
-    name: Faker::Book.genre
-  )
-end
-
-
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-# Examples:
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+list = {
+  List.create(name: "Horror"),
+  List.create(name: "Family"),
+  List.create(name: "Comedy"),
+  List.create(name: "Favorites")
+}
